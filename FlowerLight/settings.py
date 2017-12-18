d7f7bae61039dd5f888026e9177e5062e5caf0ed
SvetaZlobina <svetlanazlobina97@gmail.com>
2017-12-19 02:09:50 +0300

    Debug mode

--- FlowerLight/settings.py
+++ FlowerLight/settings.py
@@ -93,6 +93,7 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+print(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
